22e5755695086dc4c9e5f23bc400b0efa148b9cc
carolrh <66575332+carolrh@users.noreply.github.com>
2020-06-28 21:36:47 +0100

    Update main.py

--- main.py
+++ main.py
@@ -5,6 +5,9 @@ app = Flask(__name__)
 def hello_world():
     return 'Hello, World!'
 
+if __name__ == "__main__":
+    app.run(debug = os.environ.get('DEBUG') == 'True', host = '0.0.0.0', port = os.environ.get('PORT'))
+
 # @app.route('/say-bye')
 # def say_bye():
 #     return 'Goodbye!'
